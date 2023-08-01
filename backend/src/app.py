051b8cfc91cdb380004c92fd7711e05ab3c904c0
Alfonso <ajfuentest@gmail.com>
2023-08-01 10:31:09 -0600

    version sin login

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,79 +1,138 @@
-from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, APIRouter, status
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 import re
 import io
 from fastapi.responses import FileResponse, Response
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPBasicCredentials, HTTPBasic
-from passlib.context import CryptContext
-import jwt
-
-# Replace this with your actual secret key
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-# Sample user data (replace with your actual user data from the database)
-fake_users_db = {
-    "john_doe": {
-        "username": "john_doe",
-        "hashed_password": "$2b$12$3R19l1m6Oh.GvHH7sKBfKu4F4Lrl1GNbyLUJ2nXJQO9w2w4oyb1vi",  # Hashed password: "password123"
-    }
-}
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_user(username: str):
-    if username in fake_users_db:
-        user_dict = fake_users_db[username]
-        return user_dict
-
-
-def authenticate_user(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
-    user = get_user(credentials.username)
-    if user is None or not verify_password(credentials.password, user["hashed_password"]):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return user
-
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]  # front-end server
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+origins = ["http://localhost:5173"]  #front-end server
 
-router = APIRouter()
-
-@router.post("/login", tags=["authentication"])
-async def login(credentials: HTTPBasicCredentials = Depends(authenticate_user)):
-    access_token = create_access_token({"sub": credentials.username})
-    return {"access_token": access_token, "token_type": "bearer"}
-
-app.include_router(router, prefix="/users", tags=["users"])
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Data model
 class FileSchema(BaseModel):
     filename: str
     content: bytes
 
-# Your CSV processing code here...
-# (The same code you had previously)
+import io
+import pandas as pd
+import re
+
+def process_csv(file_content: bytes = File(...)):
+    df = pd.read_csv(io.BytesIO(file_content), skiprows=3)
+    df1 = pd.read_csv(io.BytesIO(file_content), skiprows=4)
+
+    headers = df1.columns[:18]
+    details = df.columns[18:36]
+    footers = df.columns[36:40]
+
+    new_df = pd.DataFrame(columns=headers)
+    new_df.columns = headers
+
+    for index, row in df.iterrows():
+        new_row = pd.Series(dtype='object')
+
+        for column in details:
+            value = row[column]
+            header = headers[details.get_loc(column)]
+            new_row[header] = value
+        new_df = pd.concat([new_df, new_row.to_frame().T], ignore_index=True)
+
+    pattern = r'\$[^\d]*(\d{1,3}(?:\.\d{3})*,\d{4})'
+
+    def extract_number(x):
+        match = re.match(pattern, x)
+        if match:
+            return float(match.group(1).replace('.', '').replace(',', '.'))
+        else:
+            try:
+                return float(x.replace('.', '').replace(',', '.'))
+            except ValueError:
+                return None
+
+    new_df['Monto Facturación'] = new_df['Monto Facturación'].apply(extract_number)
+    new_df['Costo Detalle  Facturación'] = new_df['Costo Detalle  Facturación'].apply(extract_number)
+
+    main_df = new_df.drop(columns=['Año', 'País', 'Línea Factura', 'Oportunidad (Producto Oportunidad)', 'Producto Oportunidad', 'Servicios (Producto Oportunidad)', 'Probabilidad (Oportunidad)', 'Industria (Cuenta)', 'Sub Categoría (Cuenta)', 'Propietario (Oportunidad)', 'Propietario', 'Estado (Oportunidad)'])
+
+    month_mapping = {
+        'Enero': 1,
+        'Febrero': 2,
+        'Marzo': 3,
+        'Abril': 4,
+        'Mayo': 5,
+        'Junio': 6,
+        'Julio': 7,
+        'Agosto': 8,
+        'Septiembre': 9,
+        'Octubre': 10,
+        'Noviembre': 11,
+        'Diciembre': 12
+    }
+
+    main_df['Mes Detalle'] = main_df['Mes'].str.split('-').str[1]
+    main_df['Mes Detalle'] = main_df['Mes Detalle'].map(month_mapping)
+
+    final_df = main_df.groupby(['Mes Detalle', 'Tipo de Venta (Producto Oportunidad)', 'Producto (Producto Oportunidad)', 'Cuenta']).agg({
+        'Monto Facturación': 'sum',
+        'Costo Detalle  Facturación': 'sum'
+    }).reset_index()
+
+    final_df['Monto Facturación'] = round(final_df['Monto Facturación'], 2)
+    final_df['Costo Detalle  Facturación'] = round(final_df['Costo Detalle  Facturación'], 2)
+
+    final_df['Utilidad'] = round(final_df['Monto Facturación'] - final_df['Costo Detalle  Facturación'], 2)
+    final_df['Margen %'] = round((final_df['Utilidad'] / final_df['Monto Facturación']) * 100, 2)
+
+    totals_df = final_df.groupby(['Mes Detalle', 'Tipo de Venta (Producto Oportunidad)']).agg({
+        'Monto Facturación': 'sum',
+        'Costo Detalle  Facturación': 'sum',
+        'Utilidad': 'sum',
+        'Margen %': 'mean'
+    }).reset_index()
+
+    final_df = pd.concat([final_df, totals_df], ignore_index=True)
+
+    final_df.rename(columns={'Tipo de Venta (Producto Oportunidad)': 'TOTAL Tipo de Venta'}, inplace=True)
+
+    grand_totals = final_df.groupby('Mes Detalle').agg({
+        'Monto Facturación': 'sum',
+        'Costo Detalle  Facturación': 'sum',
+        'Utilidad': 'sum',
+        'Margen %': 'mean'
+    }).reset_index()
+
+    grand_totals[['Monto Facturación', 'Costo Detalle  Facturación', 'Utilidad']] /= 2
+
+    final_df = pd.concat([final_df, grand_totals], ignore_index=True)
+
+    final_df.sort_values(by=['Mes Detalle', 'TOTAL Tipo de Venta', 'Producto (Producto Oportunidad)', 'Cuenta'], inplace=True)
+
+    month_groups = final_df.groupby('Mes Detalle')
+    final_df = pd.concat([month_group for _, month_group in month_groups], ignore_index=True)
+
+    final_df['TOTAL Tipo de Venta'] = final_df['TOTAL Tipo de Venta'].fillna('– TOTAL DEL MES – ')
+
+    final_df.reset_index(drop=True, inplace=True)
+
+    # Instead of using io.BytesIO, we use io.StringIO for CSV processing
+    processed_data_buffer = io.StringIO()
+    final_df.to_csv(processed_data_buffer, index=False, sep=',')
+
+    processed_csv_data = processed_data_buffer.getvalue()
+    processed_data_buffer.close()
+
+    return processed_csv_data
+
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
@@ -87,9 +146,8 @@ async def upload_file(file: UploadFile = File(...)):
 
     return Response(content=processed_csv_data, headers=headers)
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-#HAKDHNDAEDNNENOAIJN
+# @app.post("/process/")
+# async def process_file(file_data: FileSchema):
+#     # Get the processed CSV data as bytes
+#     processed_csv_data = process_and_return_csv(file_data.content)
+#     return processed_csv_data
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
-import { uploadFile, loginUser } from "./api";
+import { uploadFile } from "./api";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [processing, setProcessing] = useState(false);
   const [downloadLink, setDownloadLink] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [username, setUsername] = useState(""); // Add state for username
-  const [password, setPassword] = useState(""); // Add state for password
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -15,12 +12,6 @@ function App() {
 
   const handleFileUpload = async () => {
     if (!selectedFile) return;
-
-    // Check if the user is logged in before processing the file
-    if (!loggedIn) {
-      alert("Please log in first to access the file processing tool.");
-      return;
-    }
 
     setProcessing(true);
 
@@ -33,7 +24,7 @@ function App() {
       if (response.status === 200) {
         const contentType = response.headers.get("content-type");
         const blob = new Blob([response.data], { type: contentType });
-        const url = URL.createObjectURL(blob);
+         const url = URL.createObjectURL(blob);
         setDownloadLink(url);
       }
     } catch (error) {
@@ -52,63 +43,30 @@ function App() {
     }
   };
 
-  const handleLogin = async () => {
-    try {
-      // Check if both username and password are provided
-      if (!username || !password) {
-        alert("Please enter both username and password.");
-        return;
-      }
-
-      // Call the loginUser function with the provided credentials
-      const response = await loginUser(username, password);
-
-      if (response.status === 200) {
-        // Set the logged-in state to true if login is successful
-        setLoggedIn(true);
-        alert("Login successful. You can now use the file processing tool.");
-      } else {
-        // If login is unsuccessful, show an error message
-        alert("Login failed. Please check your username and password.");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <div className="container mx-auto mt-8">
       <h1 className="text-3xl font-bold mb-4">Ventas – CSV File Processor</h1>
-
-      {!loggedIn && (
-        <React.Fragment>
-          {/* Add input fields for username and password */}
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-            className="py-2 px-4 border rounded-md bg-white text-gray-700 hover:bg-gray-50 focus:outline-none focus:border-blue-500"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="py-2 px-4 border rounded-md bg-white text-gray-700 hover:bg-gray-50 focus:outline-none focus:border-blue-500"
-          />
-
-          <button
-            onClick={handleLogin}
-            className="py-2 px-4 rounded-md bg-blue-600 hover:bg-blue-700 text-white focus:outline-none focus:ring focus:ring-blue-300 mt-4"
-          >
-            Login
-          </button>
-        </React.Fragment>
-      )}
-
-      {loggedIn && (
-        // ... (previous code)
+      <input
+        type="file"
+        onChange={handleFileChange}
+        className="py-2 px-4 border rounded-md bg-white text-gray-700 hover:bg-gray-50 focus:outline-none focus:border-blue-500"
+      />
+      <button
+        onClick={handleFileUpload}
+        disabled={processing}
+        className={`py-2 px-4 rounded-md ${
+          processing ? "bg-blue-500 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"
+        } text-white focus:outline-none focus:ring focus:ring-blue-300 mt-4`}
+      >
+        {processing ? "Processing..." : "Upload & Process"}
+      </button>
+      {downloadLink && (
+        <button
+          onClick={handleDownload}
+          className="py-2 px-4 rounded-md bg-green-600 hover:bg-green-700 text-white focus:outline-none focus:ring focus:ring-green-300 mt-4"
+        >
+          Download Processed CSV
+        </button>
       )}
     </div>
   );
