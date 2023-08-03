2e30e90caf830f8ffa8924f5f6b8f857c506a853
DiegoV_GSI <operacionesgt@e-gsi.net>
2023-08-03 10:14:36 -0600

    fixed logo and centered it

--- frontend/src/components/dashboard.jsx
+++ frontend/src/components/dashboard.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { uploadFile } from "../api";
-import { Container, Card, Form, Button, Image, Col, Row } from 'react-bootstrap';
+import { Container, Card, Form, Button, Image, Col, Row, Stack } from 'react-bootstrap';
 
 const Dashboard = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [processing, setProcessing] = useState(false);
     const [downloadLink, setDownloadLink] = useState(null);
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
 
     const handleFileUpload = async () => {
         if (!selectedFile) return;
@@ -20,78 +20,81 @@ const Dashboard = () => {
             return;
         }
 
-    const formData = new FormData();
-    formData.append("file", selectedFile);
+        const formData = new FormData();
+        formData.append("file", selectedFile);
 
-    try {
-      const response = await uploadFile(formData);
+        try {
+            const response = await uploadFile(formData);
 
-      if (response.status === 200) {
-        const contentType = response.headers.get("content-type");
-        const blob = new Blob([response.data], { type: contentType });
-        const url = URL.createObjectURL(blob);
-        setDownloadLink(url);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setProcessing(false);
-    }
-  };
+            if (response.status === 200) {
+                const contentType = response.headers.get("content-type");
+                const blob = new Blob([response.data], { type: contentType });
+                const url = URL.createObjectURL(blob);
+                setDownloadLink(url);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setProcessing(false);
+        }
+    };
 
-  const handleDownload = () => {
-    if (downloadLink) {
-      const tempLink = document.createElement("a");
-      tempLink.href = downloadLink;
-      tempLink.setAttribute("download", "processed_data.csv");
-      tempLink.click();
-    }
-  };
+    const handleDownload = () => {
+        if (downloadLink) {
+            const tempLink = document.createElement("a");
+            tempLink.href = downloadLink;
+            tempLink.setAttribute("download", "processed_data.csv");
+            tempLink.click();
+        }
+    };
 
-  return (
-    <div style={{ backgroundColor: "#f0f0f0", minHeight: "100vh", padding: "20px" }}>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs={12} lg={6}>
-            <Card style={{ backgroundColor: "#ffffff", color: "#333", fontSize: 15, textAlign: "center", padding: "20px" }}>
-              <Card.Title style={{ color: "#27ae60", fontSize: "24px", marginBottom: "10px" }}>
-                <Image src="../assets/CAMI APP.png" width={"60%"} rounded />
-                <br />
-                CVS File Processor
-              </Card.Title>
-              <Card.Subtitle style={{ color: "#27ae60", fontSize: "16px", marginBottom: "20px" }}>
-                Upload your file, and download the processed data
-              </Card.Subtitle>
-              <Form>
-                <Form.Control
-                  type="file"
-                  onChange={handleFileChange}
-                />
-                <Button
-                  onClick={handleFileUpload}
-                  variant="success"
-                  disabled={processing}
-                  style={{ marginTop: "10px", width: "100%" }}
-                >
-                  {processing ? "Processing..." : "Upload & Process"}
-                </Button>
-                {downloadLink && (
-                  <Button
-                    onClick={handleDownload}
-                    variant="success"
-                    className="mt-3"
-                    style={{ width: "100%" }}
-                  >
-                    Download Processed CSV
-                  </Button>
-                )}
-              </Form>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <div style={{ backgroundColor: "#f0f0f0", minHeight: "100vh", padding: "20px" }}>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col xs={12} lg={6}>
+                        <Card style={{ backgroundColor: "#ffffff", color: "#333", fontSize: 15, textAlign: "center", padding: "20px" }}>
+                            <Card.Title style={{ color: "#27ae60", fontSize: "24px", marginBottom: "10px" }}>
+                                <Stack direction="horizontal" gap={3} style={{justifyContent: "center"}}>
+                                    <Image src="https://app.camiapp.net/assets/Tuerca-eb3d566b.svg" width={"18%"} rounded />
+                                    <Image src="https://app.camiapp.net/assets/Logo-8a7d2727.svg" width={"35%"} rounded />
+                                </Stack>
+                                <br />
+                                CVS File Processor
+                            </Card.Title>
+                            <Card.Subtitle style={{ color: "#27ae60", fontSize: "16px", marginBottom: "20px" }}>
+                                Upload your file, and download the processed data
+                            </Card.Subtitle>
+                            <Form>
+                                <Form.Control
+                                    type="file"
+                                    onChange={handleFileChange}
+                                />
+                                <Button
+                                    onClick={handleFileUpload}
+                                    variant="success"
+                                    disabled={processing}
+                                    style={{ marginTop: "10px", width: "100%" }}
+                                >
+                                    {processing ? "Processing..." : "Upload & Process"}
+                                </Button>
+                                {downloadLink && (
+                                    <Button
+                                        onClick={handleDownload}
+                                        variant="success"
+                                        className="mt-3"
+                                        style={{ width: "100%" }}
+                                    >
+                                        Download Processed CSV
+                                    </Button>
+                                )}
+                            </Form>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default Dashboard;
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -4,115 +4,118 @@ import { Container, Card, Form, Button, Image, Col, Row } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    console.log(username)
-    console.log(password)
-    try {
-      // Check if both username and password are provided
-      if (!username || !password) {
-        alert("Please enter both username and password.");
-        return;
-      }
+    const handleLogin = async () => {
+        console.log(username)
+        console.log(password)
+        try {
+            // Check if both username and password are provided
+            if (!username || !password) {
+                alert("Please enter both username and password.");
+                return;
+            }
 
-      // Call the loginUser function with the provided credentials
-      const response = await loginUser(username, password);
+            // Call the loginUser function with the provided credentials
+            const response = await loginUser(username, password);
 
-      if (response.status === 200) {
-        // Set the logged-in state to true if login is successful
-        setLoggedIn(true);
-        alert("Login successful. You can now use the file processing tool.");
-        navigate("/dashboard");
-      } else {
-        // If login is unsuccessful, show an error message
-        alert("Login failed. Please check your username and password.");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+            if (response.status === 200) {
+                // Set the logged-in state to true if login is successful
+                setLoggedIn(true);
+                alert("Login successful. You can now use the file processing tool.");
+                navigate("/dashboard");
+            } else {
+                // If login is unsuccessful, show an error message
+                alert("Login failed. Please check your username and password.");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  const styles = {
-    container: {
-      marginTop: '50px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      minHeight: '100vh',
-    },
-    card: {
-      width: '400px',
-      padding: '20px',
-      borderRadius: '10px',
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    },
-    cardTitle: {
-      fontSize: '24px',
-      color: "#2F4858",
-      marginBottom: '20px',
-      textAlign: 'center',
-    },
-    formLabel: {
-      fontWeight: 'bold',
-      marginBottom: '5px',
-    },
-    formInput: {
-      marginBottom: '20px',
-      padding: '10px',
-      borderRadius: '5px',
-      border: '1px solid #ddd',
-      width: '100%',
-    },
-    loginButton: {
-      backgroundColor: '#2F4858',
-      borderColor: '#2F4858',
-      width: '100%',
-      padding: '10px',
-      borderRadius: '5px',
-      color: '#fff',
-      fontSize: '16px',
-      fontWeight: 'bold',
-    },
-  };
+    const styles = {
+        container: {
+            marginTop: '50px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '100vh',
+        },
+        card: {
+            width: '400px',
+            padding: '20px',
+            borderRadius: '10px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        },
+        cardTitle: {
+            fontSize: '24px',
+            color: "#2F4858",
+            marginBottom: '20px',
+            textAlign: 'center',
+        },
+        formLabel: {
+            fontWeight: 'bold',
+            marginBottom: '5px',
+        },
+        formInput: {
+            marginBottom: '20px',
+            padding: '10px',
+            borderRadius: '5px',
+            border: '1px solid #ddd',
+            width: '100%',
+        },
+        loginButton: {
+            backgroundColor: '#2F4858',
+            borderColor: '#2F4858',
+            width: '100%',
+            padding: '10px',
+            borderRadius: '5px',
+            color: '#fff',
+            fontSize: '16px',
+            fontWeight: 'bold',
+        },
+    };
 
-  return (
-    <Container style={styles.container}>
-      <Card style={styles.card}>
-        <Card.Body>
-          <Card.Title style={styles.cardTitle}>
-            <Image src="../assets/CAMI APP.png" width={"60%"} rounded />
-            <br />
-            Hello! This is a Log In
-          </Card.Title>
-          <Form.Group>
-            <Form.Label style={styles.formLabel}>Email</Form.Label>
-            <Form.Control
-              type="email"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              style={styles.formInput}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label style={styles.formLabel}>Password</Form.Label>
-            <Form.Control
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={styles.formInput}
-            />
-          </Form.Group>
-          <Button variant="primary" onClick={handleLogin} style={styles.loginButton}>
-            Log In
-          </Button>
-        </Card.Body>
-      </Card>
-    </Container>
-  );
+    return (
+        <Container style={styles.container}>
+            <Card style={styles.card}>
+                <Card.Body>
+                    <Card.Title style={styles.cardTitle}>
+                        <Stack direction="horizontal" gap={3} style={{ justifyContent: "center" }}>
+                            <Image src="https://app.camiapp.net/assets/Tuerca-eb3d566b.svg" width={"18%"} rounded />
+                            <Image src="https://app.camiapp.net/assets/Logo-8a7d2727.svg" width={"35%"} rounded />
+                        </Stack>
+                        <br />
+                        Hello! This is a Log In
+                    </Card.Title>
+                    <Form.Group>
+                        <Form.Label style={styles.formLabel}>Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            style={styles.formInput}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label style={styles.formLabel}>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            style={styles.formInput}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" onClick={handleLogin} style={styles.loginButton}>
+                        Log In
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
 };
 
 export default Login;
