2748c330a168fca11044e8f4b9ef20d86e341ec7
DiegoV_GSI <operacionesgt@e-gsi.net>
2023-08-17 09:30:04 -0600

    fixes

--- frontend/src/components/analitics.jsx
+++ frontend/src/components/analitics.jsx
@@ -1,66 +1,60 @@
 import React, { useState, useEffect, PureComponent } from 'react';
-import { Card, Form, Col, Row, Accordion, Spinner, Table } from 'react-bootstrap';
+import { Card, Form, Col, Row, Accordion, Spinner, Table, Container } from 'react-bootstrap';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Analitics = () => {
   const data = [
     {
       name: 'Page A',
-      uv: 4000,
       pv: 2400,
       amt: 2400,
     },
     {
       name: 'Page B',
-      uv: 3000,
       pv: 1398,
       amt: 2210,
     },
     {
       name: 'Page C',
-      uv: 2000,
       pv: 9800,
       amt: 2290,
     },
     {
       name: 'Page D',
-      uv: 2780,
       pv: 3908,
       amt: 2000,
     },
     {
       name: 'Page E',
-      uv: 1890,
       pv: 4800,
       amt: 2181,
     },
     {
       name: 'Page F',
-      uv: 2390,
       pv: 3800,
       amt: 2500,
     },
     {
       name: 'Page G',
-      uv: 3490,
       pv: 4300,
       amt: 2100,
     },
   ];
   return (
     <Row>
-      <Col>
-        <Card>
-          <Card.Title>
+      <Col className="justify-content-md-center">
+        <Card style={{ backgroundColor: "#ffffff", color: "#333", fontSize: 15, textAlign: "center", padding: "20px", borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', }}>
+          <Card.Title style={{ color: "#27ae60", fontSize: "24px", marginBottom: "10px" }}>
             Analitics
           </Card.Title>
           <Card.Subtitle>
             Huehuehue
           </Card.Subtitle>
           <Card.Text>
+            <Container>
               <BarChart
-                width={500}
-                height={300}
+                width={800}
+                height={600}
                 data={data}
                 margin={{
                   top: 20,
@@ -72,12 +66,11 @@ const Analitics = () => {
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
-                <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
                 <Tooltip />
                 <Legend />
                 <Bar yAxisId="left" dataKey="pv" fill="#8884d8" />
-                <Bar yAxisId="right" dataKey="uv" fill="#82ca9d" />
               </BarChart>
+            </Container>
           </Card.Text>
         </Card>
       </Col>
