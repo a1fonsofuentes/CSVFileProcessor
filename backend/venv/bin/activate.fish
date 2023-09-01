4171406e776fb4dc1380070dfbd7ac4d80791be4
DiegoV_GSI <operacionesgt@e-gsi.net>
2023-09-01 12:47:11 -0600

    dockers for build

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ventas/Documents/Alfonso/SalesData/backend/venv"
+set -gx VIRTUAL_ENV "/home/carlos/Documents/cvs-transformer/CSVFileProcessor/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
